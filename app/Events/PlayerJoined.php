802620b2a4993ae2d647e9bc6302c1991730dffa
CaweAlves <contato@cawe.dev>
2024-10-29 09:01:45 -0300

    creating player joined event

---
+++ app/Events/PlayerJoined.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Game;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PlayerJoined implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public Game $game, public string $playerName)
+    {
+    }
+
+    public function broadcastOn(): Channel
+    {
+        return new Channel('game.' . $this->game->code);
+    }
+}
